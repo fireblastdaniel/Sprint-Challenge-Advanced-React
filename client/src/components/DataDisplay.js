09604ceb74eafb2dca59185d804ad407394c06d7
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-02-14 08:50:57 -0800

    makes api call to localhost and displays player data

---
+++ client/src/components/DataDisplay.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const DataDisplay = () => {
+    const [players, setPlayers] = useState([]);
+    useEffect( () => {
+        axios
+            .get('http://localhost:5000/api/players')
+            .then(response => {
+                setPlayers(response.data);
+            })
+            .catch( err => {
+                console.error(err);
+            })
+    })
+
+    return(
+        <div>
+            {
+                players.map(player => (
+                    <div>
+                        <h2>{player.name}</h2>
+                        <p>Country: {player.country}</p>
+                        <p>Searches: {player.searches}</p>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default DataDisplay;
